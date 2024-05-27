f6e56efd10f5079f9d8a3f0c8c61e62ba8df3a8d
faquanly <78623809+faquanly@users.noreply.github.com>
2024-05-27 10:28:17 -0700

    fifth commit

--- sw.js
+++ sw.js
@@ -29,29 +29,15 @@ self.addEventListener('install', function (event) {
         'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
         'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
       ];
-      await cache.addAll(RECIPE_URLs);
+
+       self.addEventListener('install', function (event) {
+        event.waitUntil(
+          caches.open(CACHE_NAME).then(function (cache) {
+            // Add all of the URLs from RECIPE_URLs to the cache when the ServiceWorker is installed
+            return cache.addAll(RECIPE_URLS);
     })()
   );
 });
-    // caches.open(CACHE_NAME).then(function (cache) {
-    //   // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
-    //   //            added to the cache when the ServiceWorker is installed
-    //   const RECIPE_URLS = [
-    //   'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
-    //   'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
-    //   'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
-    //   'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
-    //   'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
-    //   'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
-    //   ];
-    //   self.addEventListener('install', function (event) {
-    //     event.waitUntil(
-    //       caches.open(CACHE_NAME).then(function (cache) {
-    //         // Add all of the URLs from RECIPE_URLs to the cache when the ServiceWorker is installed
-    //         return cache.addAll(RECIPE_URLS);
-    //       })
-    //     );
-    //   });
 
 // Activates the service worker
 self.addEventListener('activate', function (event) {
@@ -76,33 +62,17 @@ self.addEventListener('fetch', function (event) {
 
   event.respondWith((async () => {
       let cache_name = await caches.open(CACHE_NAME);
-      let response = await cache_name.match(event.request);
+      let cachedResponse = await cache_name.match(event.request);
       // B8. If the request is in the cache, return with the cached version.
       //     Otherwise fetch the resource, add it to the cache, and return the network response.
-      if (response) {return response;
+      if (cachedResponse) {return cachedResponse;
       } else {
-        let networkResponse = await fetch(event.request);
-        await cache_name.put(event.request, networkResponse.clone());
-        return networkResponse;
+        let fetchedResponse = await fetch(event.request);
+        await cache_name.put(event.request, fetchedResponse.clone());
+        return fetchedResponse;
       }
     })()
   );
 });
-
-//   event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-//   // B8. TODO - If the request is in the cache, return with the cached version.
-//   //            Otherwise fetch the resource, add it to the cache, and return
-//   //            network response.
-//     return cache.match(event.request).then((cachedResponse) => {
-//       if (cachedResponse) {
-//         return cachedResponse;
-//       }
-//       return fetch(event.request).then((fetchedResponse) => {
-//         cache.put(event.request, fetchedResponse.clone());
-//         return fetchedResponse;
-//             });
-//         });
-//     }));
-// });
-// // }));
-// // });
+}));
+});
